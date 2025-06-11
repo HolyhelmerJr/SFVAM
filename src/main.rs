03c6768f20e26bfa2a49e344c08d3b47d883f717
holy <@c24ghn>
2025-06-11 03:06:15 +0200

    Added a beginning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::mem::uninitialized;
+use std::io::Read;
+use std::path::Path;
+use openssh::{Session, KnownHosts};
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let ssh_name: String = initlizeOptions();
+
+
+    
+}
+
+fn initlizeOptions() -> String{
+    let mut options = File::open("options.txt").unwrap();
+    let mut ssh_name = String::new();
+
+    options.read_to_string(&mut ssh_name).unwrap();
+    return ssh_name;
+}
+
+async fn create_connection(ssh_name: &String) -> Session {
 }
